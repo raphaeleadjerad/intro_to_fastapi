fb8f65f1250bb106d7b163ac26138c0a7e8250ec
raphaeleadjerad <raphaele.adjerad@gmail.com>
2022-09-06 11:45:29 +0000

    ajout test upload donnees fastapi

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
+#import pandas as pd
+import csv
+import json
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
 
+def read_dict(file):
+    with open("../src/test.json") as f:
+        df = json.load(f)
+    return df
+
+@app.on_event("startup")
+def startup_event():
+    global full_dict
+    full_dict = read_dict('test.json')
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": full_dict}
